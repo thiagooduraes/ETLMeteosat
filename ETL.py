b904cc9525e87c983ccc497981b40fa7adc9d488
thiagooduraes <thiagooduraes@gmail.com>
2024-11-08 02:14:52 -0300

    Dash-ETL Brasil Ontem

--- ETL.py
+++ ETL.py
@@ -1,48 +1,68 @@
-import meteostat
+import meteostat as ms
 import datetime as dt
 import duckdb
+import warnings
 
-def create_duckdb():
-    result = duckdb.sql("""
-    SELECT 
-    tavg as Temp_Media, tmin as Temp_Min,
-    tmax as Temp_Max,
-    prcp as Precipitacao,
-    snow as Neve,
-    wdir as Vento_Dir,
-    wspd as Vento_Med,
-    wpgt as Vento_Max,
-    pres as Pressao,
-    tsun as Tempo_Sol
-    FROM data
-    """)
+def execute_duckdb(conn):
+    query = "SELECT id as ID, station as Estacao, tavg as Temp_Media, tmin as Temp_Min, tmax as Temp_Max, prcp as Precipitacao, snow as Neve, wdir as Vento_Dir, wspd as Vento_Med, wpgt as Vento_Max, pres as Pressao, tsun as Tempo_Sol FROM weather_data"
 
-    result.show()
+    result = conn.execute(query).fetchdf()
+
+    #Sresult.show()
 
     #Salvando em arquivo parquet
-    result.write_parquet('data/measurements_summary.parquet')
+    result.to_parquet('data/measurements_summary.parquet')
 
 if __name__ == "__main__":
     import time
     
     # Set time period
     end = dt.datetime.now()
-    start = end - dt.timedelta(weeks=1)
+    start = end - dt.timedelta(days=1)
 
-    # Create Point for Montes Claros, MG
-    location = meteostat.Point(-16.7167, -43.8667, 646)
+    # Obtendo todas as estações disponíveis
+    stations = ms.Stations().region('BR')
+    # Aplicar filtros para os dados desejados (opcional)
+    # Por exemplo, filtrar por data de início de operação e localização
 
-    # Get daily data for 2018
-    data = meteostat.Daily(location, start, end)
-    data = data.fetch()
+    # Buscar as estações
+    all_stations = stations.fetch()
 
-    # Plot line chart including average, minimum and maximum temperature
-    data.plot(y=['tavg', 'tmin', 'tmax'])
-    # station, time, tavg, tmin, tmax, prcp, snow, wdir, wspd, wpgt, pres, tsun
-
+    conn = duckdb.connect('weather_data.duckdb')
+    
     print("Iniciando o processamento do arquivo.")
     start_time = time.time()
-    create_duckdb()
+
+    # Criar a tabela apenas uma vez
+    # Este comando cria a tabela, mas não insere dados, garantindo que os campos sejam configurados corretamente.
+    sample_station = ms.Daily(all_stations.index[0], start, end).fetch()  # Buscar dados para uma estação amostra
+    sample_station['id'] = all_stations.index[0]  # Adicionar a coluna de 'station' de exemplo para definir o esquema
+    sample_station['station'] = all_stations.index[0]  # Adicionar a coluna de 'city_name' de exemplo para definir o esquema
+
+    conn.execute("CREATE TABLE IF NOT EXISTS weather_data AS SELECT * FROM sample_station LIMIT 0")
+
+
+    # Iterar por cada estação e buscar dados diários
+    size = all_stations.index.size
+    count = 1
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    for station, station_info in all_stations.iterrows():
+        data = ms.Daily(station, start, end)
+        data = data.fetch()
+        if not data.empty:
+            data['id'] = station
+
+            # Obter o nome da cidade
+            data['station'] = station_info['name']
+            # Salvar no banco de dados DuckDB
+            conn.execute("INSERT INTO weather_data SELECT * FROM data")
+            print(count)
+        else:
+            print(f"Ñ {count} de {size}")
+        count = count + 1
+
+    execute_duckdb(conn=conn)
+
     took = time.time() - start_time
     
     print(f"Processamento levou {took:.2f} segundos.")
